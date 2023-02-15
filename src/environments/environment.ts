6d8956d2d6dfc09158382f42dd0171d20b592b13
Jhon Gómez Marín <gmezjhon@gmail.com>
2023-02-15 12:17:33 -0500

    Add firebase into project

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'gdi-place-app-72577',
+    appId: '1:1064435795988:web:ce4f92ce95ccceb06aff01',
+    storageBucket: 'gdi-place-app-72577.appspot.com',
+    apiKey: 'AIzaSyDckMo-wnlMTuFDdBCWNTM0bSjqbpr2qus',
+    authDomain: 'gdi-place-app-72577.firebaseapp.com',
+    messagingSenderId: '1064435795988',
+  },};
